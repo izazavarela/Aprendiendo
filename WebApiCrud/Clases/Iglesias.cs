9f66e949827f17c52b28777b42422b113086e862
IZAZA VARELA <izazavarela1995@gmail.com>
2021-06-20 17:37:27 -0500

    Agregar archivos de proyecto.

---
+++ WebApiCrud/Clases/Iglesias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCrud.Clases
+{
+    public class Iglesias
+    {
+        [Key]
+        public int PKCodigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime Fecha_Fundacion { get; set; }
+
+    }
+}
---
+++ WebApiCrud/Controllers/IglesiasController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCrud.Clases;
+using WebApiCrud.Context;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IglesiasController : ControllerBase
+    {
+        private readonly DatabaseContext Context;
+        public IglesiasController(DatabaseContext context)
+        {
+         this.Context = context;
+        }
+
+        // GET: api/<IglesiasController>
+        [HttpGet]
+        public IEnumerable<Iglesias> Get()
+        {
+            return Context.iglesias.ToList();
+        }
+
+        // GET api/<IglesiasController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<IglesiasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<IglesiasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<IglesiasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
